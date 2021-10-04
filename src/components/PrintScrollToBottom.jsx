9c0ad1de904ea5b1232921089e6d3e05c9c79b1c
romannikiforov <escaro@ukr.net>
2021-10-04 18:38:09 +0300

    ready

--- src/components/PrintScrollToBottom.jsx
+++ src/components/PrintScrollToBottom.jsx
@@ -1,20 +1,44 @@
-import {lifecycle} from "recompose"
+import { useState, useEffect, useLayoutEffect } from "react";
 
 const scroll = () => window.scrollTo(0, document.documentElement.scrollHeight)
 
-const PrintScrollToBottom = ({children}) => children
+const PrintScrollToBottom = ({children}) => {
+  const [isScrolled, setIsScrolled] = useState(false)
 
-const enhance = lifecycle({
-  componentDidMount() {
-    scroll()
-  },
-  getSnapshotBeforeUpdate() {
-    const {clientHeight, scrollTop, scrollHeight} = document.documentElement
-    return clientHeight + scrollTop < scrollHeight
-  },
-  componentDidUpdate(prevProps, prevState, isScrolledUp) {
-    if (!isScrolledUp) scroll()
-  },
-})
+  useEffect( () => {
+    scroll();
+    function onScroll(){
+      const {clientHeight, scrollHeight, scrollTop} = document.documentElement
+      setIsScrolled(clientHeight + scrollTop < scrollHeight)
+    }
+    window.addEventListener('scroll', onScroll)
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+  
+  
+  useLayoutEffect(() => {
+    if (!isScrolled) {
+      scroll()
+    }
+  })
 
-export default enhance(PrintScrollToBottom)
+  return children
+}
+
+
+
+/* const PrintScrollToBottom = ({children}) => {
+  const {clientHeight, scrollHeight} = document.documentElement
+  
+  useLayoutEffect(() => {
+    const isScrolled = clientHeight + window.pageYOffset < scrollHeight
+    if (!isScrolled) {
+      scroll()
+    }
+  })
+
+  return children
+}
+ */
+
+export default PrintScrollToBottom;
